e6a02b5b50921bfde75b4e60d759881e69537279
EdwardOkech <okechjobs@gmail.com>
2015-12-09 06:28:18 +0300

    Created a data file to hold all the scraped data

--- motoringKenya/items.py
+++ motoringKenya/items.py
@@ -1,14 +1,11 @@
-# -*- coding: utf-8 -*-
-
-# Define here the models for your scraped items
-#
-# See documentation in:
-# http://doc.scrapy.org/en/latest/topics/items.html
 
 import scrapy
 
 
-class MotoringkenyaItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class ChekiItem(scrapy.Item):
+    """
+    Define item models for checki.co.ke website
+    """
+    title = scrapy.Field()
+    desc = scrapy.Field()
+
